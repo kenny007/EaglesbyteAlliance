e86fd2a36dc6afd8325785ae965d760f46853a47
Kehinde Adebayo <press.spark@gmail.com>
2017-12-10 00:08:56 +0200

    Add Logic to LoginActivity, RegisterActivity added with it's logic
    * Design the Login and Register Screen
    * Include logo for the company

--- app/src/main/java/alliance/eaglesbyte/com/eaglesbytealliance/LoginActivity.java
+++ app/src/main/java/alliance/eaglesbyte/com/eaglesbytealliance/LoginActivity.java
@@ -1,13 +1,164 @@
 package alliance.eaglesbyte.com.eaglesbytealliance;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
 
+    //Firebase ..this has two states authenticated or non-unauthenticated
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // widgets
+    private EditText mEmail;
+    private EditText mPassword;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mProgressBar  = (ProgressBar) findViewById(R.id.progressBar);
+
+        instFirebaseAuth();
+
+        Button signIn = (Button) findViewById(R.id.email_sign_in_button);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //check if the fields are filled out
+                if(!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())){
+                    Log.d(TAG, "onClick: attempting to authenticate.");
+                   loginUser(mEmail.getText().toString(),mPassword.getText().toString());
+                }else{
+                    Toast.makeText(LoginActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        TextView register = (TextView) findViewById(R.id.link_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView resetPassword = (TextView) findViewById(R.id.forgot_password);
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView resendEmailVerification = (TextView) findViewById(R.id.resend_verification_email);
+        resendEmailVerification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        hideSoftKeyboard();
+    }
+
+    //This login registered users
+    private void loginUser(String email, String password){
+                     showDialog();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                      hideDialog();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Failed to Authenticate", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param s
+     * @return
+     */
+    private boolean isEmpty(String s){
+        return s.equals("");
+    }
+
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Firebase setup*/
+   private void instFirebaseAuth(){
+      mAuthListener = new FirebaseAuth.AuthStateListener() {
+          @Override
+          public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+              FirebaseUser user = firebaseAuth.getCurrentUser();
+
+              if(user != null){
+                  Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+              }else {
+                  Log.d(TAG, "onAuthStateChanged: signed_out");
+              }
+          }
+      };
+   }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null)
+        FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
---
+++ app/src/main/java/alliance/eaglesbyte/com/eaglesbytealliance/RegisterActivity.java
@@ -0,0 +1,145 @@
+package alliance.eaglesbyte.com.eaglesbytealliance;
+
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private static final String DOMAIN_NAME = "ttu.ee";
+
+    //widgets
+    private EditText mEmail, mPassword, mConfirmPassword;
+    private Button mRegister;
+    private ProgressBar mProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mConfirmPassword = (EditText)findViewById(R.id.input_confirm_password);
+        mRegister = (Button) findViewById(R.id.btn_register);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mRegister.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG,"onClick: attempting to register.");
+
+                //checks for null valued EditText fields
+                if(!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())
+                        && !isEmpty(mConfirmPassword.getText().toString()))
+                {
+
+                    //checks if uer has a school email address
+                    if(isValidDomain(mEmail.getText().toString())){
+                        //check if passwords match
+                        if( stringMatchUtil(mPassword.getText().toString(), mConfirmPassword.getText().toString())){
+                            registerNewEmail(mEmail.getText().toString(), mPassword.getText().toString());
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                        }
+                    } else{
+                        Toast.makeText(RegisterActivity.this, "Please register with your ttu.ee email", Toast.LENGTH_SHORT).show();
+                    }
+                } else{
+                    Toast.makeText(RegisterActivity.this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        hideSoftKeyboard();
+    }
+
+    private void registerNewEmail(String email, String password){
+        showDialog();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "onComplete: onComplete: " + task.isSuccessful());
+
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: AuthState: " + FirebaseAuth.getInstance().getCurrentUser()
+                                    .getUid());
+                            FirebaseAuth.getInstance().signOut();
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                        hideDialog();
+                    }
+                });
+
+
+    }
+    /**
+     * Returns true if the user's email contains '@ttu.ee'
+     * @param email
+     * @return
+     */
+    private boolean isValidDomain(String email){
+        Log.d(TAG, "isValidDomain: verifying email has correct domain: " + email);
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        Log.d(TAG, "isValidDomain: users domain: " + domain);
+        return domain.equals(DOMAIN_NAME);
+    }
+
+    /**
+     * Return true if @param 's1' matches @param 's2'
+     * @param s1
+     * @param s2
+     * @return
+     */
+    private boolean stringMatchUtil(String s1, String s2){
+        return s1.equals(s2);
+    }
+
+    private boolean isEmpty(String string){
+        return string.equals("") || string.equals(null);
+    }
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private  void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    //hides annoying softKeyboard
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+    /**
+     * OnClickListener for mRegister Button
+     */
+    final OnClickListener mRegister_OnClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG,"onClick: attempting to register.");
+            //Toast.makeText(RegisterActivity.this, "", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+}
